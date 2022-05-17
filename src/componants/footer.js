971f43ffa218fcfa91018837dfa184e5bb30fe1b
Idravis69 <bettoumi.idris@gmail.com>
2022-05-17 10:29:09 +0200

    Co-authored-by: Tyssoxe <Tyssoxe@users.noreply.github.com>

---
+++ src/componants/footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <div className="Footer">
+            <hr/>
+            <div class="container d-flex justify-content-center">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <p class="">Copyright &copy; 2017 All Rights Reserved by
+                            <a href="#"> Idravis and Younouss</a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Footer;
---
+++ src/componants/navbar.js
@@ -0,0 +1,15 @@
+import engrenage from '../images/engrenage.png';
+import logobuisness from '../images/logobuisness.png';
+
+function Header() {
+    return (
+    <nav class="nav Navbar">
+    <a class="nav-link" href="#"><img src={logobuisness} className="imageNav"/></a>
+    <a class="nav-link" href="#"><h1>My Finance</h1></a>
+    <a class="nav-link"><img src={engrenage} className="imageNav2"></img></a>
+  </nav>
+    );
+}
+
+
+export default Header;  
